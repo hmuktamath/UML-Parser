013b123f227e2cb78b47d51c59ffaba9494cffe1
Harsha Muktamath <harsha.muktamath@sjsu.com>
2017-04-29 08:47:32 -0700

    adding somre more files

---
+++ src/main/java/uml_parser/uml_parser/Argument.java
@@ -0,0 +1,12 @@
+package uml_parser.uml_parser;
+class Argument {
+
+	String name;
+	String type;
+
+	public Argument(String name, String type) {
+		super();
+		this.name = name;
+		this.type = type;
+	}
+}
